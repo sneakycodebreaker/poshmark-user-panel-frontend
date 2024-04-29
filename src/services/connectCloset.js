76eab230d377a5812eb554c82262f3abb9007062
Ahmed Hassan <sneakycodebreaker@gmail.com>
2024-04-29 11:06:10 +0500

    Frontend - API call reverted

--- src/services/connectCloset.js
+++ src/services/connectCloset.js
@@ -2,33 +2,15 @@ import axios from 'axios';
 
 export async function connectCloset(closetName,closetPassword){
     try {
-        // const postData = {
-        //     username: closetName,
-        //     password: closetPassword
-        //   };
-
-        // const url = 'http://173.230.151.165:3001/api/closet-login';
-        // const response = await axios.post(url,postData);
-        // return response.data;
-
         const postData = {
-          username: closetName,
-          password: closetPassword
-        };
-        
+            username: closetName,
+            password: closetPassword
+          };
+
         const url = 'http://173.230.151.165:3001/api/closet-login';
-        
-        const requestOptions = {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify(postData)
-        };
-        
-        const response = await fetch(url, requestOptions);
-        const data = await response.json();
-        return data;
+        const response = await axios.post(url,postData);
+        return response.data;
+
       } catch (error) {  
         console.error('Error:', error);
         throw error;
